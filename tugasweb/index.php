113f24423a07daffc667903ff061905106abb8b1
Ayuchandra <79451566+Ayuchandra@users.noreply.github.com>
2021-02-26 13:13:04 +0800

    Add files via upload

---
+++ tugasweb/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <link rel:"stylesheet" href="css/style.css" <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="" stylesheet" type="text/css" href="css/style.css">
+    <title>Selamat Datang di Website Chandra</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+        <a class="navbar-brand" href="#">Tari Bali</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Deskripsi<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kesenian</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artikel Terbaru</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="jumbotron" id="jumbotron">
+        <div class="container">
+            <div class="text-center">
+                <img src="media\TARIBALI.jpg"> <width="300" class="rounded-circle img-thumbnail">
+                <h1 class="display-4">Sistem Informasi Tari Bali</h1>
+                <h3 class="lead">Expressions of the soul that contain elements of beauty</h3>
+            </div>
+        </div>
+    </div>
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col text-center">
+                    <h2>Deskripsi</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-5 text-justify">
+                    <p>Selamat datang di halaman website ini, halaman ini memuat tentang kesenian yang ada di Bali yaitu khususnya Tari BALI. Tari Bali masuk dalam nominasi warisan budaya dunia tak benda oleh (United Nations Educational, Scientific Cultural Organization) UNESCO.</p>
+                </div>
+                <div class="col-md-5 text-justify">
+                    <p>Tari Bali adalah tarian yang berasal dari Bali. Tari Bali tidak selalu bergantung pada alur cerita, Tujuan utama penari Bali adalah untuk menarikan tiap tahap gerakan dan rangkaian dengan ekspresi penuh.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Artikel Menarik lain nya-->
+    <section class="Artikel Menarik lain nya bg-light" id="Artikel Menarik lain nya">
+        <div class="container">
+            <div class="row pt-5 mb-4">
+                <div class="col text-center">
+                    <h2>Artikel Tari BALI</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="media/Tari-Legong-Berasal-Dari-Bali.jpg" alt="Tari Legong">
+                        <div class="card-body">
+                            <p class="card-text">Pada aspek kebudayaan
+                                Bali memiliki beberapa jenis tari yang menjadi ciri khas di wilayah pulau Dewata, julukan Bali
+                                Salah satu tari yang ada di Bali adalah tari Legong. 
+                                Tari Legong merupakan tarian tradisional Bali yang memiliki 
+                                gerakan kompleks berupa perpaduan antara gerakan penari dengan iringan musik gamelan.
+                                Kata Legong berasal dari kata "leg" yang artinya luwes atau elastis dan 
+                                kemudian diartikan sebagai gerakan lemah gemulai.
+                                Selanjutnya kata "gong" yang memiliki arti gamelan</p>
+                                <a class="btn btn-secondary" href="#" role="button">Lihat Selengkapnya.....</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="media/Tari Kecak berasal dari Bali.jpg" alt="Tari Kecak">
+                        <div class="card-body">
+                            <p class="card-text">Tari Kecak adalah kesenian tradisional sejenis seni drama tari yang khas dari Bali
+                                Tarian tersebut menggambarkan tentang cerita Pewayangan
+                                khususnya cerita Ramayana yang dipertunjukan dengan seni gerak dan tarian
+                                Tari Kecak ini merupakan salah satu kesenian tradisional
+                                yang sangat terkenal di Bali, Selain sebagai warisan budaya,
+                                Tari Kecak ini juga menjadi salah satu daya tarik para wisatawan yang datang ke sana.
+                                Menurut sumber sejarah yang ada,Tari Kecak ini di ciptakan pada tahun 1930 oleh seniman Bali 
+                                bernama Wayan Limbak dan Walter Spies seorang pelukis dari Jerman.</p>
+                                <a class="btn btn-secondary" href="#" role="button">Lihat Selengkapnya.....</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md mb-4 text-justify">
+                    <div class="card">
+                        <img class="card-img-top" src="media/tari rejang.jpg" alt="Tari Rejang">
+                        <div class="card-body">
+                            <p class="card-text">Tarian tradisional satu ini merupakan tarian yang ditampilkan
+                                sebagai bagian dari ritual keagamaan masyarakat Hindu di Bali. Namanya adalah Tari Rejang. 
+                                ari Rejang adalah tarian tradisional masyrakat Bali dalam menyambut
+                                kedatangan serta menghibur para dewa yang datang dari Khayangan dan turun ke Bumi
+                                Tarian rejang ini secara khusus ditampilkan pada waktu berlangsungnya suatu 
+                                upacara adat atau keagamaan masyarakat Hindu di Bali. Selain sebagai salah
+                                satu warisan budaya, tarian ini juga dipercaya memiliki nilai-nilai penting di dalamnya
+                                khususnya makna spiritual, sehingga juga dipercaya sebagai tarian yang suci dan dilakukan dengan penuh rasa pengabdian.</p>
+                                <a class="btn btn-secondary" href="#" role="button">Lihat Selengkapnya.....</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+                        <div class="col-lg-3">
+                <div class="sidebar">
+                    <div class="row">
+                        <div class="berita col-lg-12">
+                            <div class="sidebar-title">
+                                <h3>Berita Lain</h3>
+                                <hr>
+                            </div>
+                            <ul>
+                                <div class="list-color">
+                                    <li><a href="#">Tari Kreasi Sanggar Seni Santhi Budaya</a></li>
+                                    <li><a href="#">PKB 2019-Lomba Baleganjur Remaja Duta Kabupaten Buleleng</a></li>
+                                    <li><a href="#">Parade Lagu Bali-Kabupaten Buleleng</a></li>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-lg-12" style="background-color: #ffffff">
+                            <div class="sidebar-title">
+                                <h3>Kegiatan</h3>
+                                <hr>
+                            </div>
+                            <ul>
+                                <div class="list-light">
+                                    <li><a href="#">Gladi Gersik di wantilan persiapan lomba Tari</a></li>
+                                    <li><a href="#">Belajar makeup bersama penari</a></li>
+                                    <li><a href="#">Menyiapkan busana tari Bali </a></li>
+                                </div>
+                            </ul>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="row justify-content-center">
+
+                <div class="col=lg-4">
+
+                    <div class="card text-black bg-primary mb-3 text-center">
+
+                        <div class="card-body">
+
+                            <h5 class="card-title">Kontak Informasi</h5>
+
+                            <p class="card-text"> https://instagram.com/preginabalii?igshid=1kuoncw9pklkj</p>
+
+                        </div>
+</body>
+</html>
+
